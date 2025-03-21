310a7029cd67c89d624e3a0bc9c8c5e3ec5ed7d2
frankmilito <frankobidike20@gmail.com>
2025-03-21 05:20:22 +0100

    update from feedback

--- lib/calculate-reading-time.ts
+++
@@ -1,7 +0,0 @@
-export function calculateReadingTime(pageCount: number, pagesPerHour: number) {
-  // Calculate total time in minutes
-  const totalMinutes = Math.ceil((pageCount / pagesPerHour) * 60)
-
-  return totalMinutes
-}
-
